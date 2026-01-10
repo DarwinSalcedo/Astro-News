7acfbbbe5cff0143add704bb8f77e2aa664520ff
DarwinSalcedo <iamdarwin.salcedo@gmail.com>
2026-01-10 14:51:55 -0300

    Activate hilt
    + internet permission

---
+++ app/src/main/java/com/astro/news/AstroNewsApp.kt
@@ -0,0 +1,7 @@
+package com.astro.news
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AstroNewsApp : Application()
--- app/src/main/java/com/astro/news/MainActivity.kt
+++ app/src/main/java/com/astro/news/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.astro.news.ui.theme.AstroNewsTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
